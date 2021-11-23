f73c1cb6c5d7a557641cf3ebf98819349310cd21
Kallum Tolkien <kallumtolkien@gmail.com>
2021-11-23 00:36:15 +0000

    create Card component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Hello from "./Hello";
+import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Hello greeting={"Hello " + "React Ninja"} />
+		<Card />
 	</React.StrictMode>,
 	document.getElementById("root"),
 );
