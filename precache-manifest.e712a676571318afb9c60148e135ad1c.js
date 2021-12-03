82ecafefb2b6a47725e30b40a1ede2dd38b7541a
Kallum Tolkien <kallumtolkien@gmail.com>
2021-12-03 13:46:33 +0000

    Updates

---
+++ precache-manifest.e712a676571318afb9c60148e135ad1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423ba3b0e0174848250e420561b8098e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ec08d27b2d0115c35007",
+    "url": "/robofriends/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "02032713acb110f2f0c9",
+    "url": "/robofriends/static/css/main.2eb33927.chunk.css"
+  },
+  {
+    "revision": "ec08d27b2d0115c35007",
+    "url": "/robofriends/static/js/2.e1b685fc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robofriends/static/js/2.e1b685fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627485cada7abdc42d0d",
+    "url": "/robofriends/static/js/3.c6b94e83.chunk.js"
+  },
+  {
+    "revision": "02032713acb110f2f0c9",
+    "url": "/robofriends/static/js/main.af1e33ca.chunk.js"
+  },
+  {
+    "revision": "8e7d61bbca9a5cac374d",
+    "url": "/robofriends/static/js/runtime-main.4866d8e2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
